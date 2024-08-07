5329494cc689422104bc92cf5b7f9c54a606b039
Margaret Mwangi <margaret.mwangi3@moringaschool.com>
2024-08-07 23:44:50 +0300

    created new file for adding transactions , using empty strings for amount and description

---
+++ src/components/TransactionForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TransactionForm() {
+    const [description,setDescription] = useState('');
+    const [amount,setAmount]=useState('');
+  
+}
+
+export default TransactionForm
