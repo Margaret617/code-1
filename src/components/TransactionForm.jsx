d33fb6bbf0d87364257f70f779e50981dddcec14
Margaret Mwangi <margaret.mwangi3@moringaschool.com>
2024-08-07 23:53:57 +0300

    added a form  for the amount and description

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -4,14 +4,21 @@ function TransactionForm() {
     const [description,setDescription] = useState('');
     const [amount,setAmount]=useState('');
   
-    const handleSubmit = (e) => {
         e.preventDefault()
         if(description && amount){
             onAdd({description , amount});
             setDescription('');
             setAmount('');
         }
-    };
+    };const handleSubmit = (e) => {
+
+        return(
+            <form onSubmit={handleSubmit}>
+                <input   type="text" value={description}  placeholder="Description" onChange={(e) => setDescription(e.target.value)}/>
+                <input type="number" value={amount}  placeholder="Amount" onChange={(e) => setAmount(e.target.value)}/>
+                <button  className="submit"type="submit">Add Transaction</button>
+            </form>
+        )
 
 
     }
