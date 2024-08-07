c36840c181f35e24e6a144d5d3b1465d618afecc
Margaret Mwangi <margaret.mwangi3@moringaschool.com>
2024-08-07 23:35:54 +0300

    new file:   src/components/SearchBar.jsx #

--- src/App.js
+++ src/App.js
@@ -2,24 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [transactions,setTransactions] = useState([]);
+  const [searchTerm,setSearchTerm]=useState('');
+
+  useEffect(() =>{
+    fetch('http://localhost:3000/transactions')
+    .then(res =>res.json())
+    .then(data =>setTransactions(data))
+    .catch(error => console.log(error));
+  } , []);
+
+  function addTransaction(transaction){
+    fetch('http://localhost:3000/transactions', {
+      method: 'POST',
+      headers:{
+  'Content-Type':'application/json',
+  'Accept':'application/json'
+    },
+    body:JSON.stringify(transaction),
+    
+  })
+  .then(res =>res.json())
+  .then(data =>{setTransactions(prevTransactions=>[data,...prevTransactions]);
+    })
+    .catch(error=>{console.log('Error adding transaction...')})
+    }
+  
+    const handleSearch =(term)=>{
+      setSearchTerm(term);
+    };
+    
+    
+    const filteredTransactions = transactions.filter(transaction=>
+      transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    return(
+      <div className='App'>
+      <h1>BANK OF FLATIRON!</h1>
+      <h2>~Your Finances , Our Priority~</h2>
+      <SearchBar onSearch={handleSearch}/>
+      <TransactionForm onAdd={addTransaction}/>
+      <TransactionTable transactions={filteredTransactions}/>
+    
+      </div>
+    )
+    
 }
 
 export default App;
