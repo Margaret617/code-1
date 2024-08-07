d88a9efc70995400b1784157a3ee5ff69d6ed3c1
Margaret Mwangi <margaret.mwangi3@moringaschool.com>
2024-08-07 23:50:17 +0300

    used e.preventDefault to prevent page from reloading after transaction has been submitted

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -4,8 +4,17 @@ function TransactionForm() {
     const [description,setDescription] = useState('');
     const [amount,setAmount]=useState('');
   
-    const handleSubmit = (e) => {}
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(description && amount){
+            onAdd({description , amount});
+            setDescription('');
+            setAmount('');
+        }
+    };
+
+
+    }
   
-}
 
 export default TransactionForm
