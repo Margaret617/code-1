be6d72ddf0be802eb02eb2dcc9866ca0fa0a6632
Margaret Mwangi <margaret.mwangi3@moringaschool.com>
2024-08-08 08:40:00 +0300

    created a table for description and amount to display the contents of db.json

---
+++ src/components/TransactionTable.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TransactionTable() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>DESCRIPTION</th>
+          <th>AMOUNT</th>
+        </tr>
+      </thead>
+      </table>
+  )
+}
+
+export default TransactionTable
